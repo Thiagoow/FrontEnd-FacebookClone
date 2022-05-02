349afe65104c783492263c7c955f9427ed3105f8
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-05-02 20:55:12 -0300

    multiWord component name rule disabled

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
   rules: {
     'no-console': 'off',
     'vue/singleline-html-element-content-newline': 'off',
+    'vue/multi-word-component-names': 'off',
     'vue/html-self-closing': 'off',
     'no-empty': 'off'
   }
